2c95290ee5e4163e14d3f4a72dec56b657bebb01
dmrishabh <mishrarishabh2462@gmail.com>
2022-06-23 19:16:06 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./components/Card";
 
-function App() {
+import { Container, Row, Col } from "reactstrap";
+
+import Axios from "axios";
+
+const App = () => {
+  const [details, setDetails] = useState({});
+  // whenever we want to send a asynchronus request then use async and await
+  const fetchDetails = async () => {
+    // const response = await Axios.get("https://randomuser.me/api/");
+    const { data } = await Axios.get("https://randomuser.me/api/");
+    // console.log("RESPONSE:", data);
+    // console.log("RESPONSE:", response);
+    // console.log("RESPONSE:", response.data);
+    // console.log("RESPONSE:", response.data.info);
+    // console.log("RESPONSE:", response.data.info.version);
+    const details = data.results[0];
+    setDetails(details);
+  };
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="p-4 App">
+      <Row>
+        <Col md={4} className="offset-md-4 mt-4">
+          <Card details={details} />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="p-5">
+          <button
+            onClick={refreshPage}
+            className="btn btn-primary"
+            style={{
+              color: "white",
+              backgroundColor: "#fc466b",
+              border: "none",
+            }}
+          >
+            New User
+          </button>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import "../App.css";
+
+import {
+  FaEnvelope,
+  FaMapMarkedAlt,
+  FaPhone,
+  FaBirthdayCake,
+} from "react-icons/fa";
+const Cards = ({ details }) => {
+  //  designing a ui that can handel api
+  const myImg = details.picture?.large;
+  const title = details.name?.title;
+  const firstName = details.name?.first;
+  const lastName = details.name?.last;
+  const city = details.location?.city;
+  const phone = details.phone;
+  const email = details.email;
+  const dob = details.dob?.age;
+
+  return (
+    <div>
+      <Card
+        className="shadow-lg rounded border border-white"
+        style={{
+          backgroundColor: "#00000000",
+        }}
+      >
+        <CardBody className="text-center">
+          <img
+            src={myImg}
+            alt="img"
+            height="150"
+            width="150"
+            className="rounded-circle img-thumbnail border-danger"
+          />
+          <CardTitle className="text-white h2">
+            Hey I'm
+            <span className="pe-2 ps-2">{title}</span>
+            <span className="pe-2">{firstName}</span>
+            <span>{lastName}</span>
+          </CardTitle>
+          <CardText>
+            <FaMapMarkedAlt className="me-2" />
+            {city}
+            <p>
+              <FaPhone className="me-2" />
+              {phone}
+              <br />
+              <FaEnvelope className="me-2" />
+              {email}
+              <br />
+              <span className="bg-warning mt-2 btn text-center border border-primary rounded-pill text-white h2">
+                <FaBirthdayCake className="me-2" />
+                {dob}
+              </span>
+            </p>
+          </CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Cards;
